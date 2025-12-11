193ae33d385c2b475529507c2a524ca0386c3f4f
Muhammad Abdul Rehman Wahla <abdulrehmanwahla845@gmail.com>
2025-12-11 23:38:30 +0500

    changed exports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",      // <--- REQUIRED: Makes it a Static Site (Free)
+  images: {
+    unoptimized: true,   // <--- REQUIRED: Fixes images on the Free plan
+  },
 };
 
 export default nextConfig;
